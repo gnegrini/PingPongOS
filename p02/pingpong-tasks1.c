ff48ca0e411f61deca477c4ffdfc0a656093d742
Henrique <hkreuzner@hotmail.com>
2019-08-20 13:48:27 -0300

    teste

--- p02/pingpong-tasks1.c
+++ p02/pingpong-tasks1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "pingpong.h"
-
+//oi
 // operating system check
 #if defined(_WIN32) || (!defined(__unix__) && !defined(__unix) && (!defined(__APPLE__) || !defined(__MACH__)))
 #warning Este codigo foi planejado para ambientes UNIX (LInux, *BSD, MacOS). A compilacao e execucao em outros ambientes e responsabilidade do usuario.
