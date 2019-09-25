d2c77a5d6d1b3ebb052f0d601405eb11dd4b645c
Giovane Negrini <giovanenegrini@gmail.com>
2019-09-25 01:16:55 -0300

    implementado p4  escalonador

---
+++ p04/pingpong.c
@@ -0,0 +1,268 @@
+#include "datatypes.h" // estruturas de dados necessárias
+#include "stdio.h"
+//#define DEBUG
+#include "queue.h"
+#include "pingpong.h"
+
+task_t task_main;
+task_t task_dispatcher;
+task_t *task_atual;
+int next_id;
+
+char *stack;
+
+task_t *ready_queue;
+
+int userTasks;
+
+// funções gerais ==============================================================
+void dispatcher_body();
+
+task_t *scheduler();
+
+// Inicializa o sistema operacional; deve ser chamada no inicio do main()
+void pingpong_init()
+{
+    /* desativa o buffer da saida padrao (stdout), usado pela função printf */
+    setvbuf(stdout, 0, _IONBF, 0);
+
+    //atribui id 0 a task main
+    task_main.tid = 0;
+    next_id = 1;
+
+    //salva o contexto atual em ContextMain
+    getcontext(&(task_main.context));
+
+    task_main.prev = NULL;
+    task_main.next = NULL;
+
+    task_atual = &task_main;
+
+    //uma sempre esta executando
+    userTasks = -1;
+
+    task_create(&task_dispatcher, dispatcher_body, "NULL");
+
+#ifdef DEBUG
+    printf("SO inicializado\n");
+#endif
+}
+
+// gerência de tarefas =========================================================
+
+// Cria uma nova tarefa. Retorna um ID> 0 ou erro.
+int task_create(task_t *task,               // descritor da nova tarefa
+                void (*start_func)(void *), // funcao corpo da tarefa
+                void *arg)                  // argumentos para a tarefa
+                {
+
+    getcontext(&(task->context));
+
+    stack = malloc(STACKSIZE);
+    if (stack)
+    {
+        task->context.uc_stack.ss_sp = stack;
+        task->context.uc_stack.ss_size = STACKSIZE;
+        task->context.uc_stack.ss_flags = 0;
+        task->context.uc_link = 0;
+    }
+    else
+    {
+        perror("Erro na criacão da pilha: \n");
+        return (-1);
+    }
+
+    task->prev = NULL;
+    task->next = NULL;
+    task->status = Normal;
+    task->prio = 0;
+    task->prio_d = 0;
+
+    //talvez temos que checar o retorno da make context para retornar se deu erro
+    makecontext(&(task->context), (void *)(*start_func), 1, arg);
+
+    //atribui id para a task e atualiza o topo
+    task->tid = next_id;
+    next_id++;
+
+    userTasks++;
+
+    //adiciona nova tarefa no final da fila
+    if (task != &task_dispatcher)
+        queue_append((queue_t **)&ready_queue, (queue_t *)task);
+
+#ifdef DEBUG
+    printf("task_create: criou tarefa %d\n", task->tid);
+#endif
+
+    return task->tid;
+}
+
+// alterna a execução para a tarefa indicada
+int task_switch(task_t *task)
+{
+    task_t *task_old = task_atual;
+    task_atual = task;
+
+#ifdef DEBUG
+    printf("task_switch: trocando contexto %d -> %d\n", task_old->tid, task_atual->tid);
+#endif
+
+    int rt = swapcontext(&(task_old->context), &(task_atual->context));
+    // deu ruim
+    if (rt == -1)
+        return rt;
+    else
+        return 0;
+}
+
+// Termina a tarefa corrente, indicando um valor de status encerramento
+void task_exit(int exitCode)
+{
+#ifdef DEBUG
+    printf("task_exit: tarefa %d sendo encerrada\n", task_atual->tid);
+#endif
+
+    userTasks--;
+
+    if (task_atual == &task_dispatcher){
+
+        task_switch(&task_main);
+    }else
+    {
+        task_atual->status = Finnished;
+        task_switch(&task_dispatcher);
+        
+    }
+}
+
+// retorna o identificador da tarefa corrente (main eh 0)
+int task_id()
+{
+#ifdef DEBUG
+    printf("task_id: tarefa corrente %d\n", task_atual->tid);
+#endif
+
+    return task_atual->tid;
+}
+
+////////////// IMPLEMENTAR NA PROXIMA SEMANA /////////////
+
+// suspende uma tarefa, retirando-a de sua fila atual, adicionando-a à fila
+// queue e mudando seu estado para "suspensa"; usa a tarefa atual se task==NULL
+void task_suspend(task_t *task, task_t **queue);
+
+// acorda uma tarefa, retirando-a de sua fila atual, adicionando-a à fila de
+// tarefas prontas ("ready queue") e mudando seu estado para "pronta"
+void task_resume(task_t *task);
+
+/////////////////////////////////////////////////////////////
+
+void dispatcher_body(void *arg)
+{
+    task_t *next;
+    while (userTasks > 0)
+    {
+        next = scheduler();
+        if (next)
+        {
+            //... // ações antes de lançar a tarefa "next", se houverem
+
+            queue_remove((queue_t **)&ready_queue, (queue_t *)next);
+            task_switch(next); // transfere controle para a tarefa "next"
+            
+            //... // ações após retornar da tarefa "next", se houverem
+            if(next->status == Finnished)
+                free(next->context.uc_stack.ss_sp);
+
+        }
+    }
+    task_exit(0); // encerra a tarefa dispatcher
+}
+
+task_t *scheduler()
+{
+
+    //não há tarefas
+    if (queue_size((queue_t *)ready_queue) == 0)
+    {
+        printf("SchedErr: fila de prontas vazias\n"); 
+        return NULL;
+    }
+
+
+    //pegamos a primeira tarefa da fila para comecar e comparar prioridade
+    task_t *task_retorno = (task_t *) ready_queue;
+
+    //navegar a fila até encontrar a tarefa com maior prio_d
+    
+    task_t *atual = (task_t*) ready_queue;
+       
+    do
+    {                       
+        if(atual->prio_d < task_retorno->prio_d){
+            task_retorno = atual;
+        }
+
+        atual = atual->next;
+        
+    } while (atual!= ready_queue);
+    
+    
+    //envelhecer todas as outras tarefas
+    atual = task_retorno->next;
+    do
+    {                       
+        
+        atual->prio_d --;
+
+        atual = atual->next;
+        
+    } while (atual!= task_retorno);
+
+    //rejuvenescer a tarefa retorno
+    task_retorno->prio_d = task_retorno->prio;    
+
+    return task_retorno;
+}
+
+void task_yield()
+{
+
+#ifdef DEBUG
+    printf("task_yield: tarefa corrente %d\n", task_atual->tid);
+#endif
+
+    if (task_atual != &task_dispatcher && task_atual != &task_main)
+    {
+        queue_append((queue_t **)&ready_queue, (queue_t *)task_atual);
+    }
+
+    task_switch(&task_dispatcher);
+
+
+}
+
+// define a prioridade estática de uma tarefa (ou a tarefa atual)
+void task_setprio (task_t *task, int prio){
+    if(task == NULL){
+        task_atual->prio = prio;
+        task_atual->prio_d = prio;
+    }else
+    {
+        task->prio = prio;
+        task->prio_d = prio;
+    }
+    
+}
+
+// retorna a prioridade estática de uma tarefa (ou a tarefa atual)
+int task_getprio (task_t *task){
+    if(task == NULL){
+        return task_atual->prio;
+    }else
+    {
+        return task->prio;
+    }
+    
+}
---
+++ p04/queue.c
@@ -0,0 +1,212 @@
+//------------------------------------------------------------------------------
+// Definição e operações em uma fila genérica.
+// Implementação por Giovane Negrini e Henrique Kreuzner
+// Sistemas Operacionais - 2019.1
+//------------------------------------------------------------------------------
+
+#include "queue.h"
+#include <stdio.h>
+
+
+//------------------------------------------------------------------------------
+// Insere um elemento no final da fila.
+// Condicoes a verificar, gerando msgs de erro:
+// - a fila deve existir
+// - o elemento deve existir
+// - o elemento nao deve estar em outra fila
+
+void queue_append (queue_t **queue, queue_t *elem)
+{
+    if (queue == NULL)
+    {
+        printf("E: Fila nao existente\n");
+        return;
+    }
+    
+    
+    if (elem == NULL)
+    {
+        printf("E: Elemento nao existe\n");
+        return;
+    }
+
+    if (elem->prev != NULL || elem->next != NULL)
+    {
+        printf("E: Elemento esta em outra fila\n");
+        return;
+    }
+    
+    queue_t *atual = *queue;
+
+    if (*queue == NULL)
+    {
+        
+        // First aponta para o elemento
+        *queue = elem;
+
+        // Elemento aponta para ele mesmo
+        elem->next = elem;
+        elem->prev = elem;
+    }
+
+    // Percorre o elemento ate o ultimo elemento
+    else
+    {        
+        do    
+        {
+            atual = atual->next;
+        } while (atual->next != *queue);
+
+        // Faz o novo apontar para o anterior
+        elem->prev = atual;
+
+        // Faz o novo apontar para o primeiro
+        elem->next = *queue;
+
+        // Faz o anterior apontar para o novo
+        atual->next = elem;
+
+        // Faz o inicio apontar para o final
+        (*queue)->prev = elem;
+    }    
+    
+}
+
+//------------------------------------------------------------------------------
+// Conta o numero de elementos na fila
+// Retorno: numero de elementos na fila
+int queue_size (queue_t *queue)
+{
+    // Se a fila estiver vazia
+    if(queue == NULL)
+    {
+        return 0;
+    }
+
+        
+    int counter = 0;
+    queue_t *atual = queue;    
+    
+    // Incrementa o contador e passa para o proximo elemento
+    do
+    {        
+        counter++;
+        atual = atual->next;
+    } while (atual != queue);    
+    
+    return counter;
+
+}
+
+
+//------------------------------------------------------------------------------
+// Remove o elemento indicado da fila, sem o destruir.
+// Condicoes a verificar, gerando msgs de erro:
+// - a fila deve existir
+// - a fila nao deve estar vazia
+// - o elemento deve existir
+// - o elemento deve pertencer a fila indicada
+// Retorno: apontador para o elemento removido, ou NULL se erro
+
+queue_t *queue_remove (queue_t **queue, queue_t *elem)
+{
+    if (queue == NULL)
+    {
+        printf("E: Fila nao existe\n");
+        return NULL;
+    }
+    
+    if(*queue == NULL)
+    {
+        printf("E: Fila vazia\n");
+        return NULL;
+    }
+
+    if(elem == NULL)
+    {
+        printf("E: Elemento nao existente\n");
+        return NULL;
+    }
+    
+    // Fila com mais elementos
+            
+    queue_t *atual = *queue;
+    
+    // Procura o elemento
+    while (atual!= elem)
+    {
+        atual = atual->next;
+        
+        // Se fez uma volta completa
+        if (atual == *queue) {
+            printf("E: Elemento nao esta na fila\n");
+            return NULL;
+        }
+        
+    }
+
+    //           Realiza a remocao
+    // Faz anterior apontar pro proximo
+    (atual->prev)->next = atual->next;
+
+    // Faz proximo apontar pro anterior
+    (atual->next)->prev = atual->prev;
+
+    // Caso seja o primeiro da fila, corrige o ponteiro de inicio
+    if (*queue == elem)
+    {
+        // Fila com somente um elemento
+        if (elem->next == elem)
+        {
+            *queue = NULL;
+        }
+        else
+        {
+            *queue = elem->next;
+        }
+        
+    }
+
+    // Anula os next e prox do elemento
+    elem->next = NULL;
+    elem->prev = NULL;        
+
+    return atual;    
+
+}
+
+//------------------------------------------------------------------------------
+// Percorre a fila e imprime na tela seu conteúdo. A impressão de cada
+// elemento é feita por uma função externa, definida pelo programa que
+// usa a biblioteca. Essa função deve ter o seguinte protótipo:
+//
+// void print_elem (void *ptr) ; // ptr aponta para o elemento a imprimir
+
+void queue_print (char *name, queue_t *queue, void print_elem (void*) )
+{
+    
+
+    if (queue == NULL)
+    {
+        printf("%s[]\n", name);        
+        return;
+    }
+    
+
+    queue_t *atual = queue;
+    
+    printf("%s[", name);
+    
+    // Procura o elemento
+    do
+    {               
+        print_elem(atual);
+        printf(" ");
+        atual = atual->next;
+        
+    } while (atual!= queue);
+
+    printf("]\n");
+    
+    
+}
